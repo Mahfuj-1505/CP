66d45356157d31a9db99d6556edf5862e6ed3de6
Mahfuj-1505 <145141326+Mahfuj-1505@users.noreply.github.com>
2025-07-30 21:41:17 +0600

    Update problem.cpp

--- problem.cpp
+++ problem.cpp
@@ -38,48 +38,6 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    TestCase {
-        string str;
-        cin >> str;
-
-        if (!isDifficult(str)) {
-            cout << str << endl;
-            continue;
-        }
-
-        int count[26] = {};
-        for (char c : str) count[c - 'A']++;
-
-        string result = "";
-
-        for (int i = 0; i < 26; ++i) {
-            if ('A' + i != 'F' && 'A' + i != 'N' && 'A' + i != 'T') {
-                result.append(count[i], 'A' + i);
-                count[i] = 0;
-            }
-        }
-
-        int f = count['F' - 'A'];
-        int n = count['N' - 'A'];
-        int t = count['T' - 'A'];
-
-        while (f || n || t) {
-            if (f > 0) {
-                result += 'F';
-                f--;
-            }
-            if (n > 0) {
-                result += 'N';
-                n--;
-            }
-            if (t > 0) {
-                result += 'T';
-                t--;
-            }
-        }
-
-        cout << result << endl;
-    }
 
     return 0;
 }
